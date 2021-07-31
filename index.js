f7da5c2356f38f35dd0e3880b2e4862f4e94a610
iamrivera <a3m3rivera@gmail.com>
2021-07-30 21:18:15 -0700

    commit tree bb14e723b2d407901126c9482e122aa3fe0b98d4

--- index.js
+++ index.js
@@ -1,1 +1,49 @@
 // Your code here
+
+class Polygon {
+    constructor(sides) {
+        this.sides = sides   
+    }
+        
+        get countSides() {
+            return this.sides.length
+        }
+
+        get perimeter() {
+            return this.sides.reduce(function(total, side){
+                return total+side
+            })
+        }  
+};
+
+class Triangle extends Polygon {
+    //inherits constructors
+
+    get isValid() {
+        let trianglez = this.countSides == 3 && this.sides[0]+this.sides[1] > this.sides[2] && this.sides[0]+this.sides[2] > this.sides[1] && this.sides[1]+this.sides[2] > this.sides[0]
+
+        if (trianglez) {
+            return !!trianglez
+        } else {
+            return !!trianglez
+        }
+
+    }
+}
+
+class Square extends Polygon {
+
+    get isValid() {
+        let squarez = this.countSides == 4 && this.sides[0] == this.sides[1] && this.sides[0] == this.sides[2] && this.sides[0] == this.sides[3]
+
+        if (squarez) {
+            return !!squarez
+        } else {
+            return !!squarez
+        }
+    }
+
+    get area() {
+        return this.sides[0]**2
+    }
+}
